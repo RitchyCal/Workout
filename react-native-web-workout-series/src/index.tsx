e0b44256e7345c997d4cc610275435223bd8438e
richy163 <ritchycal@gmail.com>
2020-05-13 15:08:33 -0400

    changed to react file fixing component

--- react-native-web-workout-series/src/index.tsx
+++ react-native-web-workout-series/src/index.tsx
@@ -1,21 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { AppRegistry } from "react-native";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// register the app
 AppRegistry.registerComponent("App", () => App);
 
 AppRegistry.runApplication("App", {
   initialProps: {},
   rootTag: document.getElementById("root"),
 });
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
