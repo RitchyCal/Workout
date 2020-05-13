49be9d2b672337b6115cc086da793f7988e39d04
richy163 <ritchycal@gmail.com>
2020-05-13 15:54:49 -0400

    fixed package.json file

--- react-native-web-workout-series/src/App.tsx
+++ react-native-web-workout-series/src/App.tsx
@@ -1,12 +1,8 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 
 const App: React.FC = () => {
-  return (
-    <View>
-      <Text>hello from react native web</Text>
-    </View>
-  );
+  return <Text>hello from react native web</Text>;
 };
 
 export default App;
