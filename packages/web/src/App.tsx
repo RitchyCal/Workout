55ffcde42d07207630778c677c0f87a2d8eb7be8
richy163 <ritchycal@gmail.com>
2020-05-13 18:27:29 -0400

    add web files and moved them into packages

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const App: React.FC = ({}) => {
+  return (
+    <View>
+      <Text>hello from react </Text>
+    </View>
+  );
+};
+
+export default App;
