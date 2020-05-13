e8426079d9e8e2fec2706dd47245f3cb5e23ae36
richy163 <ritchycal@gmail.com>
2020-05-13 19:05:48 -0400

    added files to common folder

---
+++ packages/common/src/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, StyleSheet, Text, useState, View } from "react-native";
+
+
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{count}</Text>
+      <Button title="increment" onPress={() => setCount(count + 1)} />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
+  },
+});
